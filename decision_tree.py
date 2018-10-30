8281bc000d8a42010aab3e4fadda3b16ea68bb47
FerminAMC <a01209665@itesm.mx>
2018-10-30 16:56:16 -0600

    Best node selected

--- decision_tree.py
+++ decision_tree.py
@@ -74,8 +74,6 @@ class Node():
         self.children = None
         self.answer = False
         self.position = 0
-        def __lt__(self, other):
-            return self.entropy < other.entropy
 
     def is_leaf(self):
         if(self.children == None):
@@ -91,87 +89,57 @@ class Node():
             for i in self.values:
                 for j in answer_node.values:
                     self.values[i].update({j:0})
-
-            #print(self.values[data_table[0][0]])
             for i in range(len(data_table)):
                 for j in range(len(data_table[i])):
                     if(self.position == j):
                         self.values[data_table[i][j]][data_table[i][table_width]] += 1
-
         else: # We are working with the answer node
             for i in self.values:
                 self.values[i] = 0
-
             for i in range(len(data_table)):
                 self.values[data_table[i][table_width]] += 1
 
-        print(self.values)
 
-    def calc_entropy(self, nodes):
-        if(len(nodes) == 1):
+    def calc_entropy(self):
+        if(self.answer == True):
             entropy = 0
             total = 0
-            if(nodes[0].answer == True):
-                for i in nodes[0].values:
-                    total += nodes[0].values[i]
-                for i in nodes[0].values:
-                    entropy -= (nodes[0].values[i]/total) * math.log((nodes[0].values[i]/total), 2)
-            else:
-                for i in nodes[0].values:
-                    for j in nodes[0].values[i]:
-                        total += nodes[0].values[i][j]
-                for i in nodes[0].values:
-                    i_total = 0
-                    for j in nodes[0].values[i]:
-                        i_total += nodes[0].values[i][j]
-                    entropy -= (i_total/total) * math.log((i_total/total), 2)
+            for i in self.values:
+                total += self.values[i]
+            for i in self.values:
+                entropy -= (self.values[i]/total) * math.log((self.values[i]/total), 2)
             return(entropy)
-        else: # The first node given must be the answer node
+        else:
             total_entropy = 0
             total = 0
-            for i in nodes[1].values:
-                for j in nodes[1].values[i]:
-                    total += nodes[1].values[i][j]
-            for i in nodes[1].values:
+            for i in self.values:
+                for j in self.values[i]:
+                    total += self.values[i][j]
+            for i in self.values:
                 i_total = 0
                 probability = 0
                 entropy = 0
-                for j in nodes[1].values[i]:
-                    i_total += nodes[1].values[i][j]
-                for j in nodes[1].values[i]:
-                    probability = nodes[1].values[i][j]/i_total
+                for j in self.values[i]:
+                    i_total += self.values[i][j]
+                for j in self.values[i]:
+                    probability = self.values[i][j]/i_total
                     if(probability > 0):
                         entropy -= probability * math.log(probability, 2)
                 total_entropy += (i_total/total)*entropy
             return(total_entropy)
 
-    def calc_entropy2(self, node):
-        if(node.answer == True):
-            entropy = 0
-            total = 0
-            for i in node.values:
-                total += node.values[i]
-            for i in node.values:
-                entropy -= (node.values[i]/total) * math.log((node.values[i]/total), 2)
-            return(entropy)
-        else: # The first node given must be the answer node
-            total_entropy = 0
-            total = 0
-            for i in node.values:
-                for j in node.values[i]:
-                    total += node.values[i][j]
-            for i in node.values:
-                i_total = 0
-                probability = 0
-                entropy = 0
-                for j in node.values[i]:
-                    i_total += node.values[i][j]
-                for j in node.values[i]:
-                    probability = node.values[i][j]/i_total
-                    if(probability > 0):
-                        entropy -= probability * math.log(probability, 2)
-                total_entropy += (i_total/total)*entropy
-            return(total_entropy)
+    def select_best(self, nodes):
+        best = None
+        info_gained = 0
+        answer_entropy = 0
+        for node in nodes:
+            if(node.answer == True):
+                answer_entropy = node.calc_entropy()
+        for node in nodes:
+            if(info_gained < answer_entropy - node.calc_entropy()):
+                info_gained = answer_entropy - node.calc_entropy()
+                best = node
+        return best
 
 if __name__ == "__main__":
     values = [5,4,5]
@@ -204,4 +172,4 @@ if __name__ == "__main__":
     ]
     root.count_values(data_table, answer)
     answer.count_values(data_table, answer)
-    print(answer.calc_entropy2(answer))
+    Node().select_best(nodes)
