edf4b75f0bee0a769f97be04c45cfdf74782baf5
ferminamc1 <abcdefg@gmail.com>
2018-11-02 18:02:35 +0000

    Comments

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,8 @@
 # A great source for understanding how to calculate the Entropy and
 # Information gained
 # https://www.saedsayad.com/decision_tree.htm
+
+# Import libraries
 import math
 import fileinput
 import re
@@ -11,6 +13,7 @@ class Node():
         self.attributes = []
         self.position = 0
 
+# Calculation of entropy
 def calc_entropy(nodes, data_table):
     entropy = 0
     answer_node = nodes[len(nodes)-1]
@@ -24,6 +27,8 @@ def calc_entropy(nodes, data_table):
             entropy -= (counter[attribute] / len(data_table)) * math.log((counter[attribute] / len(data_table)), 2)
     return entropy
 
+
+# Information gain for every node
 def calc_info_gain(nodes, data_table, att, ent):
     ifo_gain = 0
     entropy = 0
@@ -35,11 +40,13 @@ def calc_info_gain(nodes, data_table, att, ent):
     info_gain = ent - entropy
     return info_gain
 
+# Splitting data to format it for recursion
 def split_data(data_table, attribute, position):
     split_data = [row for row in data_table if attribute == row[position]]
     #pp.pprint(split_data)
     return split_data
 
+# Building the tree
 def id3(nodes, data_table, entropy, depth):
     tabs = "  " * depth
     if entropy == 0:
@@ -59,7 +66,7 @@ def id3(nodes, data_table, entropy, depth):
                 recursive_entropy = calc_entropy(nodes, split)
                 if(len(split) > 0):
                     id3(nodes, split, recursive_entropy, depth + 1)
-
+# Parsing the node
 def parse_node(string, nodes):
     # Format the attributes to add it to the dictionary of attributes
     string = string.split(' ')
