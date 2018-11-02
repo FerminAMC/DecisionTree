95435bdb2e2f4e3ff90725915d78a40a21797df2
FerminAMC <a01209665@itesm.mx>
2018-11-02 11:43:39 -0600

    decision tree finished

--- decision_tree.py
+++ decision_tree.py
@@ -33,7 +33,7 @@ import math
 '''
 @attribute input must be parsed this way:
 name = "outlook"
-values = ['sunny': 0, 'overcast': 0, 'rainy': 0]
+values = {'sunny': {}, 'overcast': 0, 'rainy': 0}
 if(!next_line contains '@attribute') answer = True
 position = number of @attribute
 
@@ -55,33 +55,13 @@ data_table = [
     ["rainy","mild","high","TRUE","no"]
 ]
 '''
-# Node class definition
-'''
-name = "outlook"
-values = {'sunny': {'yes': 3, 'no': 2}, 'overcast': {'yes': 4, 'no': 0}, 'rainy': {'yes': 2, 'no': 3}}
-answer = False
-position = 0
-
-name = "play"
-values = {'yes': 9, 'no': 5}
-answer = True
-position = 4
-'''
 class Node():
     def __init__(self):
         self.name = ""
         self.values = {}
-        self.children = None
+        self.children = {}
         self.answer = False
         self.position = 0
-
-    def is_leaf(self):
-        if(self.children == None):
-            return True
-        return False
-
-    def set_values(self, values):
-        self.values = values
 
     def count_values(self, data_table, answer_node):
         table_width = len(data_table[0])-1
@@ -98,7 +78,7 @@ class Node():
                 self.values[i] = 0
             for i in range(len(data_table)):
                 self.values[data_table[i][table_width]] += 1
-
+        #print(self.name + ": " + str(self.values))
 
     def calc_entropy(self):
         if(self.answer == True):
@@ -139,21 +119,56 @@ class Node():
             if(info_gained < answer_entropy - node.calc_entropy()):
                 info_gained = answer_entropy - node.calc_entropy()
                 best = node
+        #print(best.name + ": " + str(best.values))
+        #print(info_gained)
         return best
 
+    def set_children(self):
+        for val in self.values:
+            self.children.update({val: Node()})
+
+    def split_data(self, data_table):
+        pass
+
+    def is_answer(self, data_table):
+        possible_answer = data_table[0][len(data_table-1)]
+        for i in range(len(data_table)):
+            if(possible_answer != data_table[i][len(data_table-1)]):
+                return False
+        print("ANSWER: " + possible_answer)
+        return True
+
+def id3(nodes, data_table):
+    pass
+
 if __name__ == "__main__":
-    values = [5,4,5]
     root = Node()
-    answer = Node()
-    nodes = [answer, root]
-    root.name = "outlook"
-    root.values = {'sunny': {}, 'overcast': {}, 'rainy': {}}
-    root.answer = False
-    root.position = 0
-    answer.name = "play"
-    answer.values = {'yes': {}, 'no': {}}
-    answer.answer = True
-    answer.position = 4
+    outlook = Node()
+    temp = Node()
+    humidity = Node()
+    windy = Node()
+    play = Node()
+    nodes = [outlook, temp, humidity, windy, play]
+    outlook.name = "outlook"
+    outlook.values = {'sunny': {}, 'overcast': {}, 'rainy': {}}
+    outlook.answer = False
+    outlook.position = 0
+    temp.name = "temperature"
+    temp.values = {'hot': {}, 'mild': {}, 'cool': {}}
+    temp.answer = False
+    temp.position = 1
+    humidity.name = "humidity"
+    humidity.values = {'high': {}, 'normal': {}}
+    humidity.answer = False
+    humidity.position = 2
+    windy.name = "windy"
+    windy.values = {'TRUE': {}, 'FALSE': {}}
+    windy.answer = False
+    windy.position = 3
+    play.name = "play"
+    play.values = {'yes': {}, 'no': {}}
+    play.answer = True
+    play.position = 4
     data_table = [
         ["sunny","hot","high","FALSE","no"],
         ["sunny","hot","high","TRUE","no"],
@@ -170,6 +185,11 @@ if __name__ == "__main__":
         ["overcast","hot","normal","FALSE","yes"],
         ["rainy","mild","high","TRUE","no"]
     ]
-    root.count_values(data_table, answer)
-    answer.count_values(data_table, answer)
-    Node().select_best(nodes)
+
+
+    for node in nodes:
+        node.count_values(data_table, nodes[len(nodes)-1])
+    root = Node().select_best(nodes)
+    root.set_children()
+    for child in root.children:
+        print(root.children[child])
